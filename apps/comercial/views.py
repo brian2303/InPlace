c3268e9593aa630284f5bc5e15c7d1fd0f2b5062
GersonM <gersonmorales69@gmail.com>
2020-08-25 00:20:44 -0500

    Cada APP tiene su carpeta Templates, se modificó los productos del área Inventario a Producción

--- apps/comercial/views.py
+++ apps/comercial/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def index_comercial(request):
-    return render(request,'comercial/index.html')
+    return render(request,'index.html')
