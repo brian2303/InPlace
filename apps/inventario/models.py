20a29be3fa940d4c92de527055547ffaf99cad3d
GersonM <gersonmorales69@gmail.com>
2020-08-25 00:50:42 -0500

    Se crea los modelos de Proveedor y TelefonoProveedor

--- apps/inventario/models.py
+++ apps/inventario/models.py
@@ -3,3 +3,25 @@ from datetime import datetime
 
 # Create your models here.
 
+class Proveedor(models.Model):
+    nombre=models.CharField(max_length=25)
+    ciudad=models.CharField(max_length=25)
+    direccion=models.CharField(max_length=25)
+    
+    class Meta:
+        verbose_name='proveedor'
+        verbose_name_plural='proveedores'
+        db_table='proveedor'
+
+class TelefonoProveedor(models.Model):
+    numero_telefono=models.CharField(max_length=11)
+    proveedor=models.ForeignKey(
+        Proveedor, 
+        on_delete=models.CASCADE, 
+        related_name='telefono'
+    )
+
+    class Meta:
+        verbose_name='telefono'
+        verbose_name_plural='telefonos'
+        db_table='telefono_proveedor'
