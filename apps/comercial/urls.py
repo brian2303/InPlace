b1b75d928ebf46f8c191606af19c0d4cfe69fa2a
Brian Olarte <brayan_andrey23@hotmail.com>
2020-08-27 18:20:04 -0500

    terminando el crud de clientes

--- apps/comercial/urls.py
+++ apps/comercial/urls.py
@@ -5,5 +5,7 @@ from apps.comercial.views.clientes.views import *
 
 urlpatterns = [
     path('clientes/lista',ClienteListView.as_view(),name='clientes_lista'),
-    path('clientes/crear',ClienteCreateView.as_view(),name='cliente_crear')
+    path('clientes/crear',ClienteCreateView.as_view(),name='cliente_crear'),
+    path('clientes/editar/<int:pk>',ClienteUpdateView.as_view(),name='cliente_editar'),
+    path('clientes/eliminar/<int:pk>',ClienteDeleteView.as_view(),name='cliente_eliminar')
 ]
