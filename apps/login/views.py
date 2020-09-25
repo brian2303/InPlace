9633e86a244abdcda89203148161ee36afdbb57c
Brian Olarte <brayan_andrey23@hotmail.com>
2020-09-24 23:17:18 -0500

    agregando el grafico de ventas

--- apps/login/views.py
+++ apps/login/views.py
@@ -4,6 +4,10 @@ from django.shortcuts import redirect
 from django.views.generic import TemplateView
 from django.utils.decorators import method_decorator
 from django.contrib.auth.mixins import LoginRequiredMixin
+from apps.comercial.models import Ventas
+from datetime import datetime
+from django.db.models.functions import Coalesce
+from django.db.models import Sum
 # Create your views here.
 
 class LoginFormView(LoginView):
@@ -23,9 +27,18 @@ class LoginFormView(LoginView):
 class DashboardView(LoginRequiredMixin,TemplateView):
     template_name = "base/dashboard.html"
     
+    def get_sales_data(self):
+        data = []
+        year = datetime.now().year
+        for m in range(1,13):
+            total = Ventas.objects.filter(fecha__year=year,fecha__month=m).aggregate(
+                r=Coalesce(Sum('total'),0)).get('r')
+            data.append(float(total))
+        return data
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["title"] = 'Dashboard' 
+        context["title"] = 'Dashboard'
+        context['ventas_mes_a_mes'] = self.get_sales_data() 
         return context
     
