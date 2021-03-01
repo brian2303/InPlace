241a7ea159c48a002ff699dc673f4d634bfe8eb5
mrgerson88 <gersonmorales69@gmail.com>
2021-02-28 21:13:43 -0500

    Despliegue v1

--- InPlace/wsgi.py
+++ InPlace/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'InPlace.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'InPlace.settings.dev')
 
 application = get_wsgi_application()
