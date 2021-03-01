8a4ee6692e6852855f816570a9a3d1b1cb2b91b5
mrgerson88 <gersonmorales69@gmail.com>
2021-02-28 22:44:04 -0500

    Despliegue v2

--- InPlace/settings.py
+++ InPlace/settings.py
@@ -166,7 +166,7 @@ MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 
-EMAIL_HOST = 'smtp.googlemail.com'
+EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'heippi.prueba@gmail.com'
 EMAIL_HOST_PASSWORD = 'Admin1234*'
